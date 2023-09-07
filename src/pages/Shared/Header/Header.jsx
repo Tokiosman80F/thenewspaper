3a7a26ec34a82cd604653286200ea317ef656f8b
Tokiosman80F <tokiosman96@gmail.com>
2023-09-07 22:43:46 +0600

    navbar is created

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,9 @@
 import moment from "moment/moment";
-import { Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
+import NavigationBar from "./NavigationBar";
 const Header = () => {
     return (
-      <Container className='text-center mt-5'>
+      <div className='text-center mt-5'>
         <h1 className='text-uppercase mb-3'>
           The-
           <span className='text-danger border border-2 border-secondary px-2'>
@@ -12,7 +12,7 @@ const Header = () => {
         </h1>
         <p className='text-secondary'>Journalism Without Fear or Favour</p>
         <h5>{moment().format("dddd,MMMM Do,YYYY")}</h5>
-        <div className="d-flex ">
+        <div className="d-flex mt-4">
           <span className='bg-danger text-white border-0 px-4 py-2 rounded'>
             Latest
           </span>
@@ -21,7 +21,8 @@ const Header = () => {
             Highlights: Germany vs Spain as...  
           </Marquee>
         </div>
-      </Container>
+        <NavigationBar></NavigationBar>
+      </div>
     );
 }
 
--- src/pages/Shared/Header/NavigationBar.jsx
+++ src/pages/Shared/Header/NavigationBar.jsx
@@ -1,11 +1,26 @@
-
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { FaCircleUser } from "react-icons/fa6";
 
 const NavigationBar = () => {
-    return (
-        <div>
-            this is nav bar
-        </div>
-    )
-}
+  return (
+    <Navbar collapseOnSelect expand='lg' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mx-auto '>
+            <Nav>Home </Nav>
+            <Nav className='mx-3'>About</Nav>
+            <Nav>Carrer</Nav>
+          </Nav>
+          <Nav>
+            <Nav className="mx-auto">
+              <Button className='bg-secondary border-0 mx-2'>Login</Button>
+            </Nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
